3196bfc64dde35de89bf1587a96cca2cb9eb1e64
Maxim Doroshenko <m.s.doroshenko@gmail.com>
2025-07-23 12:28:11 +0300

    Initial Commit

---
+++ OptiUniverse/OptiUniverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptiUniverseApp.swift
+//  OptiUniverse
+//
+//  Created by max on 23.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct OptiUniverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
