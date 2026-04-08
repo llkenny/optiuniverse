831ef5c4ab90ff3ebeb5af939f0a8d11a4ce2b39
Maxim Doroshenko <m.s.doroshenko@gmail.com>
2026-04-08 19:49:06 +0300

    Add TitleSectionView

---
+++ OptiUniverse/UIComponents/TitleSection/TitleSectionView.swift
@@ -0,0 +1,35 @@
+//
+//  TitleSectionView.swift
+//  OptiUniverse
+//
+//  Created by max on 08.04.2026.
+//
+
+import SwiftUI
+
+struct TitleSectionView: View {
+    @State var name: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Hi \(name),")
+                .font(.system(size: 18, weight: .light))
+                .foregroundStyle(Color(.midEmphasized))
+            HStack {
+                Text("Where do you wanna go?")
+                    .font(.system(size: 32, weight: .bold))
+                    .foregroundStyle(Color(.highEmphasized))
+                    .lineLimit(2)
+                Spacer(minLength: 60)
+            }
+        }
+    }
+}
+
+#Preview {
+    VStack {
+        TitleSectionView(name: "Stranger")
+        Spacer()
+    }
+    .padding(.horizontal)
+}
