8e4d027ff2736f0c0e2be4213e1e2898f6b4b2dd
Maxim Doroshenko <m.s.doroshenko@gmail.com>
2026-04-19 22:27:44 +0700

    Add destination JSON loading

---
+++ OptiUniverse/UIComponents/DestinationListView/DestinationObject.swift
@@ -0,0 +1,15 @@
+//
+//  DestinationObject.swift
+//  OptiUniverse
+//
+//  Created by Codex on 19.04.2026.
+//
+
+import Foundation
+
+struct DestinationObject: Decodable {
+    let id: UUID
+    let title: String
+    let subtitle: String
+    let imageName: String
+}
