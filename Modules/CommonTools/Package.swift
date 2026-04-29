f9042141267827edb3b609cb4832db71614f0e4a
Maxim Doroshenko <m.s.doroshenko@gmail.com>
2026-04-29 16:42:53 +0700

    Add CommonTools module

--- Modules/CommonTools/Package.swift
+++ Modules/CommonTools/Package.swift
@@ -4,27 +4,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "MetalModule",
+    name: "CommonTools",
     platforms: [
         .iOS(.v18)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "MetalModule",
-            targets: ["MetalModule"]
+            name: "CommonTools",
+            targets: ["CommonTools"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MetalModule",
-            dependencies: ["CommonTools"]
+            name: "CommonTools"
         ),
         .testTarget(
-            name: "MetalModuleTests",
-            dependencies: ["MetalModule"]
+            name: "CommonToolsTests",
+            dependencies: ["CommonTools"]
         ),
     ],
     swiftLanguageModes: [.v6]
--- Modules/MetalModule/Sources/MetalModule/Models/SolarSystemLoader.swift
+++ Modules/MetalModule/Sources/MetalModule/Models/SolarSystemLoader.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CommonTools
 
 final class SolarSystemLoader {
     enum Constants {
