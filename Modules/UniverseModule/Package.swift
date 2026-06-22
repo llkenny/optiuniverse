76818151793705727235ab0bfafbdb38f8474be0
Maxim Doroshenko <m.s.doroshenko@gmail.com>
2026-06-22 12:47:27 +0300

    Extract Moon

--- Modules/UniverseModule/Package.swift
+++ Modules/UniverseModule/Package.swift
@@ -33,6 +33,7 @@ let package = Package(
                 .copy("Assets/Models/high_resolution_solar_system.usdz"),
                 .copy("Assets/Models/Sun.usdz"),
                 .copy("Assets/Models/Mercury.usdz"),
+                .copy("Assets/Models/Moon.usdz"),
                 .copy("Assets/Models/Neptune.usdz"),
                 .process("Assets/Models/celestial_assets.json"),
                 .process("Assets/Environment"),
