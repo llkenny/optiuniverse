2f2597e78d866c79d92d6922ede6b88f5729f7ef
Maxim Doroshenko <m.s.doroshenko@gmail.com>
2026-06-25 18:13:22 +0300

    Separate orbit and spin axes for planetary snapshots

--- Modules/UniverseModule/Sources/UniverseModule/RealityKit/SceneSnapshot/CelestialBodySnapshot.swift
+++ Modules/UniverseModule/Sources/UniverseModule/RealityKit/SceneSnapshot/CelestialBodySnapshot.swift
@@ -10,8 +10,28 @@ import simd
 struct CelestialBodySnapshot: Sendable {
     let planetName: String
     let baseModelMatrix: float4x4
+    let orbitTransformMatrix: float4x4
+    let visualRotationMatrix: float4x4
     let normalizedScale: Float
     let framingRadius: Float
     let surfaceRadius: Float
     let worldPosition: SIMD3<Float>
+
+    init(planetName: String,
+         baseModelMatrix: float4x4,
+         orbitTransformMatrix: float4x4 = matrix_identity_float4x4,
+         visualRotationMatrix: float4x4 = matrix_identity_float4x4,
+         normalizedScale: Float,
+         framingRadius: Float,
+         surfaceRadius: Float,
+         worldPosition: SIMD3<Float>) {
+        self.planetName = planetName
+        self.baseModelMatrix = baseModelMatrix
+        self.orbitTransformMatrix = orbitTransformMatrix
+        self.visualRotationMatrix = visualRotationMatrix
+        self.normalizedScale = normalizedScale
+        self.framingRadius = framingRadius
+        self.surfaceRadius = surfaceRadius
+        self.worldPosition = worldPosition
+    }
 }
